c00cfc178937bb90f6cd321a8863ded7df524c82
Michael Bellard <michaelbellard2@my.solacc.edu>
2019-02-03 20:10:01 -0600

    Replaced token.json with token.ini.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const token = NDA4MDk4NDQ0ODg5MDMwNjU3.Dzkmcw.UYc-uMbFFhZy6lEQSXSA-9IuCUo;
 const discord = require('discord.js');
-const client = new Discord.Client();
+const fs = require('fs');
+
+const client = new discord.Client();
+const token = ini.parse(fs.readFileSync('./token.ini', 'utf-8'));
 
 client.on('ready', () => {
-
+    console.log('Disco-Bot is ready!');
+    client.channels.find(x => x.name === 'test').send('Hello! I\'m now connected!');
 });
+
+client.login(token);
