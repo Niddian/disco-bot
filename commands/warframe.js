dd5d69dd6a49cbd4d1ffbac2a964cac9bf396b63
Michael Bellard <michaelbellard2@my.solacc.edu>
2019-02-27 10:26:01 -0600

    Skeleton of JSON filtering using modNames.

--- commands/warframe.js
+++ commands/warframe.js
@@ -5,25 +5,36 @@ module.exports.run = async (client, msg, args) => {
     let json = await fetch("https://drops.warframestat.us/data/modLocations.json")
         .then(res => res.json());
     json = json['modLocations'];
-    let embed = new Discord.RichEmbed()
-        .setTitle("Warframe")
-        .setColor(0xAA00AA)
-        .setImage();
+    let embed = new Discord.RichEmbed();
+    let enemies = [];
+    let chance = [];
     // console.log(json['modLocations'].filter(d => d.modName === 'Arcane Healing'));
     for (data in json) {
         if (!json.hasOwnProperty(data)) { continue; }
         for (mods in json[data]) {
             if (!json[data].hasOwnProperty(mods)) { continue; }
-            // console.log(json[data][mods])
+            if (!(json[data][mods] === args.join(" "))) { continue; }
+
+            for (enemy in json[data]['enemies']) {
+                if (!json[data]['enemies'].hasOwnProperty(enemy)) { continue; }
+                // combo.push({ enemy: json[data]['enemies'][enemy]['enemyName'], chance: json[data]['enemies'][enemy]['chance'] });
+                enemies.push(json[data]['enemies'][enemy]['enemyName']);
+                chance.push(json[data]['enemies'][enemy]['chance']);
+            }
         }
     }
+    if (enemies.length === 0) {
+        msg.channel.send(`'${args.join(' ')}' does not match any known mod names.`);
+        return;
+    }
+    let combo = enemies.map((itm, i) => {
+        return [itm, chance[i] + '%'];
+    }).join('\n');
 
-    // Object.keys(json).forEach(key => {
-    //     console.log(json[key]);
-    // })
-    // Object.entries(json['modLocations']).forEach(([key, value]) => {
-    //     console.log(value)
-    // });
+    embed.setTitle(args.join(" "))
+        .setColor(0xAA00AA)
+        .setImage()
+        .setDescription(combo.split(',').join(' - '));
     msg.channel.send(embed);
 }
 
