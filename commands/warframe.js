aa3eec0230cace9e4ebe1e5580e32986d725a387
Michael Bellard <michaelbellard2@my.solacc.edu>
2019-02-27 14:11:31 -0600

    Clean-up and added ignorecase TODO.

--- commands/warframe.js
+++ commands/warframe.js
@@ -2,22 +2,24 @@ const Discord = require('discord.js');
 const fetch = require('node-fetch');
 
 module.exports.run = async (client, msg, args) => {
+    if (args.length === 0) { return; }
     let json = await fetch("https://drops.warframestat.us/data/modLocations.json")
-        .then(res => res.json());
-    json = json['modLocations'];
-    let embed = new Discord.RichEmbed();
+        .then(res => res.json())
+        .then(data => data['modLocations']);
     let enemies = [];
     let chance = [];
-    // console.log(json['modLocations'].filter(d => d.modName === 'Arcane Healing'));
+    // console.log(json.filter(d => d.modName === 'Arcane Healing'));
+
+    // TODO: Mod names are Pascal Case. Change the checks to ignore case.
     for (data in json) {
         if (!json.hasOwnProperty(data)) { continue; }
         for (mods in json[data]) {
             if (!json[data].hasOwnProperty(mods)) { continue; }
+            console.log(JSON.stringify(json[data][mods]).toLowerCase() + '\n');
             if (!(json[data][mods] === args.join(" "))) { continue; }
 
             for (enemy in json[data]['enemies']) {
                 if (!json[data]['enemies'].hasOwnProperty(enemy)) { continue; }
-                // combo.push({ enemy: json[data]['enemies'][enemy]['enemyName'], chance: json[data]['enemies'][enemy]['chance'] });
                 enemies.push(json[data]['enemies'][enemy]['enemyName']);
                 chance.push(json[data]['enemies'][enemy]['chance']);
             }
@@ -31,7 +33,8 @@ module.exports.run = async (client, msg, args) => {
         return [itm, chance[i] + '%'];
     }).join('\n');
 
-    embed.setTitle(args.join(" "))
+    let embed = new Discord.RichEmbed()
+        .setTitle(args.join(" "))
         .setColor(0xAA00AA)
         .setImage()
         .setDescription(combo.split(',').join(' - '));
